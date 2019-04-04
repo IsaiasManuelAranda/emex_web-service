c3e9d73773ee9d3d98d7d11f820a4621ed2c97a7
IsaiasArandaLeal <1717110610@utectulancingo.edu.mx>
2019-04-04 13:45:09 -0600

    Puerto agregado en parametros

--- application/models/config.py
+++ application/models/config.py
@@ -4,6 +4,7 @@ db_host = 'thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	'
 db_name = 'c2buiuqoxcowv0sz'
 db_user = 'zao0bi9cjgnbn459'
 db_pw = 'chlzfkwhzjrmvrovi1ay'
+port = '3306'
 
 db = web.database(
     dbn='mysql',
@@ -11,4 +12,5 @@ db = web.database(
     db=db_name,
     user=db_user,
     pw=db_pw
+    port='3306'
     )
