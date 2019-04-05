0fd7ac6301ae62fcefa64adef9f01081223b027f
IsaiasArandaLeal <1717110610@utectulancingo.edu.mx>
2019-04-04 22:27:58 -0600

    Web service funcionando

--- app.py
+++ app.py
@@ -39,5 +39,5 @@ urls = (
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
-    web.config.debug = False
+    web.config.debug = True
     app.run()
--- application/models/config.py
+++ application/models/config.py
@@ -1,9 +1,9 @@
 import web
 
-db_host = 'thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	'
+db_host = 'thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
 db_name = 'c2buiuqoxcowv0sz'
 db_user = 'zao0bi9cjgnbn459'
-db_pw = 'chlzfkwhzjrmvrovi1ay'
+db_pw = 'lzfkwhzjrmvrovi1'
 port = '3306'
 
 db = web.database(
@@ -12,5 +12,4 @@ db = web.database(
     db=db_name,
     user=db_user,
     pw=db_pw
-    port='3306'
     )
